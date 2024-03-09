7fe716d78b9843e376add05f3e38e2a2d043579b
prathikshavenkat29 <prathikshavenkat29@gmail.com>
2024-03-09 03:54:47 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://ethicsinfotech.in/uploads/slider/e-commerce-website.png" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://backend-ten-eta-88.vercel.app"
